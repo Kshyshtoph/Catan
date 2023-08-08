6c412c2b54b43cd4d393ec8bacebe8ec4d996ef0
Kshyshtoph <baumkrzysztof@gmail.com>
2023-08-08 09:16:01 +0200

    add missing utils file

---
+++ utils.js
@@ -0,0 +1,15 @@
+const spliceRandomItem = (array) => {
+    return array.splice(
+        Math.floor(Math.random() * array.length),
+        1
+    )
+}
+
+
+const getCenter = (matrix, i1, i2) => {
+    const [p1x, p1y] = matrix[i1]
+    const [p2x, p2y] = matrix[i2]
+    const x = (p1x + p2x) / 2
+    const y = (p1y + p2y) / 2
+    return [x, y]
+}
